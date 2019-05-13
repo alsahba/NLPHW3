6b8c14efa4b98691ca2b18427f87d6db111e49a4
alsahba <alisahinbalikci@gmail.com>
2019-05-13 16:12:35 +0300

    Completion of Assignment

---
+++ DocumentClassification.py
@@ -0,0 +1,69 @@
+import multiprocessing
+from warnings import simplefilter
+
+import gensim
+import nltk
+import pandas as pd
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+from nltk.corpus import stopwords
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+assert gensim.models.doc2vec.FAST_VERSION > -1, "This will be painfully slow otherwise"
+simplefilter(action='ignore', category=FutureWarning)
+
+
+class DocumentClassification(object):
+    # Number of cores got and csv file read into data frame with help of pandas library.
+    cores = multiprocessing.cpu_count()
+    data_frame = pd.read_csv('tagged_plots_movielens.csv')
+    stop_words = stopwords.words('english')
+
+    # Train and test data separated between each other.
+    train = data_frame.iloc[:2000, :]
+    test = data_frame.iloc[2000:, :]
+
+    # This method divides documents to sentences, then divides sentences to words.
+    def textTokenizer(self, text):
+        return list([word.lower() for sentence in nltk.sent_tokenize(text.strip())
+                     for word in nltk.word_tokenize(sentence) if
+                     (len(word) >= 2 and word not in (self.stop_words))])
+
+    # Sentences and their tags divided from tagged documents and returned.
+    def vec_for_learning(self, model, documents):
+        targets, sentences = zip(*[(doc.tags[0], model.infer_vector(doc.words, steps=20)) for doc in documents])
+        return targets, sentences
+
+    # Train data and test data created with respect to train/test slices of data frame.
+    # Model of doc2vec is created. And vocabulary created.
+    # Doc2vec model trained with train data that we created before.
+    # Train and test tags and sentences got and created variables.
+    # LogisticRegression model is created.
+    # Train variables' result points fitted in a line with an optimal equation.
+    # Prediction estimation doing with test variables.
+    # Accuracy of predictions printed in the console window.
+    def classify(self):
+        print("\nDocument classification started...")
+        tagged_train_data = self.train.apply(
+            lambda df: TaggedDocument(words=self.textTokenizer(str(df['plot'])), tags=[df['tag']]),
+            axis=1)
+
+        tagged_test_data = self.test.apply(
+            lambda df: TaggedDocument(words=self.textTokenizer(str(df['plot'])), tags=[df['tag']]),
+            axis=1)
+
+        model_dbow = Doc2Vec(dm=0, window=30, vector_size=50, workers=self.cores)
+        model_dbow.build_vocab(tagged_train_data)
+        model_dbow.train(tagged_train_data, total_examples=model_dbow.corpus_count, epochs=20)
+
+        y_train, X_train = self.vec_for_learning(model_dbow, tagged_train_data)
+        y_test, X_test = self.vec_for_learning(model_dbow, tagged_test_data)
+
+        logreg = LogisticRegression(n_jobs=5, solver='lbfgs')
+        logreg.fit(X_train, y_train)
+        y_pred = logreg.predict(X_test)
+
+        print(
+            'Accuracy of classification with doc2vec task is {:.2f} percent.'.format(
+                (accuracy_score(y_test, y_pred)) * 100))
--- Main.py
+++ Main.py
@@ -1,49 +1,8 @@
-import numpy as np
-from gensim.models import KeyedVectors
+from Analogy import Analogy
+from DocumentClassification import DocumentClassification
 
+analogy = Analogy()
+doc_classification = DocumentClassification()
 
-word2vec_model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin',binary=True, limit=300000)
-vector_magnitudes = np.apply_along_axis(np.linalg.norm, 1, word2vec_model.vectors)
-
-def calculateCosine(test_vector):
-    dot_products = np.dot(word2vec_model.vectors, test_vector)
-    test_vector_magnitude = np.linalg.norm(test_vector)
-    similarities = dot_products / (vector_magnitudes * test_vector_magnitude)
-    return similarities
-
-def organizeAnalogyTestFile():
-    test_lines = []
-    [test_lines.append(t_line.split()) for t_line in open("analogy_small.txt", "r").readlines() if len(t_line.strip().split()) == 4]
-    return test_lines
-
-
-def analogyCorrecter(test_words, similarity_calculations, sorted_array, index):
-    m = np.where(similarity_calculations == sorted_array[index])
-    predicted_word = word2vec_model.index2word[m[0][0]]
-    if (predicted_word == test_words[0]) or (predicted_word == test_words[1]) or (predicted_word == test_words[2]):
-        predicted_word = analogyCorrecter(test_words, similarity_calculations,sorted_array, index + 1)
-    return predicted_word
-
-
-def analogyTask():
-    analogy_test_lines = organizeAnalogyTestFile()
-    correct_predict = 0
-    # todo try excepti kaldir
-    for test_words in analogy_test_lines:
-        try:
-            test_vector = word2vec_model[test_words[2]] + word2vec_model[test_words[1]] - word2vec_model[test_words[0]]
-            similarity_calculations = calculateCosine(test_vector)
-            predicted_word = word2vec_model.index2word[np.argmax(similarity_calculations)]
-            if ((predicted_word == test_words[0]) or (predicted_word == test_words[1]) or (predicted_word == test_words[2])):
-                sorted_array = np.sort(similarity_calculations, 0)[::-1]
-                predicted_word = analogyCorrecter(test_words, similarity_calculations, sorted_array, 1)
-
-            if predicted_word == test_words[3]:
-                correct_predict = correct_predict + 1
-        except:
-            print("henak")
-
-    print("Accuracy of analogy task is {}".format((correct_predict/len(analogy_test_lines)) * 100))
-
-
-# analogyTask()
+analogy.findAnalogy()
+doc_classification.classify()
